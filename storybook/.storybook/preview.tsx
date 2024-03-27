929a8cb3477b32ce2ca1419dacb6837e249c6386
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-27 10:09:40 -0400

    got memory router vite thing happily next to next

--- storybook/.storybook/preview.tsx
+++ storybook/.storybook/preview.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
 
+export const decorators = [
+  (Story) => (
+    <MemoryRouter>
+      <Story />
+    </MemoryRouter>
+  ),
+];
 const preview: Preview = {
   parameters: {
     controls: {
