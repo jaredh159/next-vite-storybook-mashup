0003491e6837e1f1e5fb772085065d55e089eec9
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-27 09:54:50 -0400

    delete some redundent next stuff

--- storybook/src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home(): any {
-  return <h1> storybook</h1>;
-}
--- storybook/src/stories/Button.stories.ts
+++ storybook/src/stories/Button.stories.ts
@@ -1,25 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-// import { Button } from './Button';
 import FeatureCard from '../../../site/components/FeatureCard';
 
 const meta = {
-  title: 'Example/Button',
+  title: 'Example/FeatureCard',
   component: FeatureCard,
-  parameters: {
-    layout: 'centered',
-  },
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-  args: { onClick: fn() },
+  parameters: { layout: 'fullscreen' },
 } satisfies Meta<typeof FeatureCard>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     primary: true,
